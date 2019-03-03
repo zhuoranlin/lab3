738db268133320efea8980378b4ad8d37bf80d41
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 20:46:17 +0800

    commit

--- queryEntries.php
+++ queryEntries.php
@@ -8,8 +8,7 @@ mysqli_select_db($conn, 'h3523520') or die('Error! '. mysqli_error($conn));
 print "<p>".$_GET["show"]."</p>";
 print "<p>".$_GET["value"]."</p>";
 
-$s = $_GET["value"];
-$query = "SELECT * FROM stockList WHERE stockcode = '$s';";
+$query = "SELECT * FROM stockList WHERE stockcode = '$_GET["value"]';";
 
 //Execute SQL query
 $result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
