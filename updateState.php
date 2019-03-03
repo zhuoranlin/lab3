4b1283d947f8be1392d6304f3315cdcb724a1f85
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 22:22:10 +0800

    1st

--- updateState.php
+++ updateState.php
@@ -1,18 +1,5 @@
 <?php
-//Connect to database
-$conn=mysqli_connect('sophia.cs.hku.hk', 'h3523520', 'FuDMbGLZ') or die('Error! '. mysqli_error($conn));
-//Select database
-mysqli_select_db($conn, 'h3523520') or die('Error! '. mysqli_error($conn));
-//Construct your SQL query here
 $value = $_GET['newValue'];
-$query = "update stockList set status= '$value' where id=".$_GET['id'];
-
-//Execute SQL query
-$result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
-while($row = mysqli_fetch_array($result)) {
-        print "$row['status']";
-}
-
-mysqli_close($conn);
+print $value;
 
 ?>
