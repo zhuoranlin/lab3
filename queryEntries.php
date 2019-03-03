5a3fbdd048ec508f978bda33bf4fb57e045b8fea
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 22:11:54 +0800

    1st

--- queryEntries.php
+++ queryEntries.php
@@ -5,9 +5,6 @@ $conn=mysqli_connect('sophia.cs.hku.hk', 'h3523520', 'FuDMbGLZ') or die('Error! 
 mysqli_select_db($conn, 'h3523520') or die('Error! '. mysqli_error($conn));
 //Construct your SQL query here
 
-print "<p>".$_GET["show"]."</p>";
-print "<p>".$_GET["value"]."</p>";
-
 $s = $_GET["value"];
 if ($_GET["show"]=="all"){
 	$query = 'select * from stockList;';
@@ -23,7 +20,7 @@ if ($_GET["show"]=="category"){
 $result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
 while($row = mysqli_fetch_array($result)) {
         print "<div id=".$row['id'].">";
-        print "<span>".$row['status']."</span><h3>".$row['stockname']." (".$row['category'].")</h3><h5>(".$row['stockcode'].") on ".$row['date']."</h5>";
+        print "<span onclick = \"changeState(this)\">".$row['status']."</span><h3>".$row['stockname']." (".$row['category'].")</h3><h5>(".$row['stockcode'].") on ".$row['date']."</h5>";
         print "</div>";
 }
 
