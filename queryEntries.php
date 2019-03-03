3221836c278285a037d7550c6a49da4feeb4cd27
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 19:24:36 +0800

    commit

--- queryEntries.php
+++ queryEntries.php
@@ -8,7 +8,9 @@ mysqli_select_db($conn, 'h3523520') or die('Error! '. mysqli_error($conn));
 print "<p>".$_GET["show"]."</p>";
 print "<p>".$_GET["value"]."</p>";
 
-$query = 'select * from stockList;';
+if ($_GET["show"]=="all"){
+	$query = 'select * from stockList;';
+}
 
 //Execute SQL query
 $result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
