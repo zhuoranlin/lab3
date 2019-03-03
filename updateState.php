79da2cdc47f6a96d3df5db134abd8ef61c5afbd0
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 23:17:09 +0800

    1st

--- updateState.php
+++ updateState.php
@@ -10,10 +10,10 @@ print $value;
 $query = "UPDATE stockList SET status = '$value' WHERE id = '$id';";
 
 //Execute SQL query
-$result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
-while($row = mysqli_fetch_array($result)) {
-        print $row['status'];
-}
+//$result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
+//while($row = mysqli_fetch_array($result)) {
+//        print $row['status'];
+//}
 
 mysqli_close($conn);
 
