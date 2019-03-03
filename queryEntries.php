fdd71e2985e816b26d73f518043bf616042e7b86
Zhuoran Lin <zhrlin1025@gmail.com>
2019-03-03 18:03:54 +0800

    8

--- queryEntries.php
+++ queryEntries.php
@@ -4,7 +4,18 @@ $conn=mysqli_connect('sophia.cs.hku.hk', 'h3523520', 'FuDMbGLZ') or die('Error! 
 //Select database
 mysqli_select_db($conn, 'h3523520') or die('Error! '. mysqli_error($conn));
 //Construct your SQL query here
-$query = 'select * from stockList';
+
+
+if ($_GET["show"]=="all"){
+	$query = 'select * from stockList;';
+}
+if ($_GET["show"]=="stockcode"){
+	$query = 'select * from stockList where stockcode = $_GET["value"];';
+}
+if ($_GET["show"]=="category"){
+	$query = 'select * from stockList;';
+}
+
 //Execute SQL query
 $result = mysqli_query($conn, $query) or die('Error! '. mysql_error($conn));
 while($row = mysqli_fetch_array($result)) {
